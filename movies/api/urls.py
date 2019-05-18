d6c7ce89ef68c1d72fc889601d0d00f4dae01c85
tugayclml <tugay.celmeli.7@gmail.com>
2019-05-18 16:58:23 +0300

    Deneme

---
+++ movies/api/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from api.views import MovieCreateView, MovieDetailView
+
+urlpatterns = [
+    url(r'^movies/$', MovieCreateView.as_view(), name='movies'),
+    url(r'^movies/(?P<id>[0-9]+)$', MovieDetailView.as_view(), name='detail'),
+]
